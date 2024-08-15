3e897c03b23cf85a84a7c116dfe856614f740f41
Ebube-Ochemba <ebube17@gmail.com>
2024-08-15 11:02:05 +0100

    task 2 completed

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """A Redis client Module"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Any, Callable, Optional, Union
 
 
 class Cache:
@@ -16,6 +16,27 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis"""
         key = str(uuid4())
-        client = self._redis
-        client.set(key, data)
+        self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """Retrieve data from Redis and optionally convert it"""
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """ Converts bytes to string"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Converts bytes to integers"""
+        return int(data)
