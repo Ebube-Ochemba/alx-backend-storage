5c5df50ea046ee30d63c0112258bfdd54bcf72c7
Ebube-Ochemba <ebube17@gmail.com>
2024-08-16 00:17:19 +0100

    first attempt

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""A requests caching module"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+# Create a Redis client
+client = redis.Redis()
+
+
+def url_access_count(func: Callable) -> Callable:
+    """Count the number of times a function is called"""
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """Wraps called method and increments its call count
+        in redis before execution"""
+        client.incr(f"count:{url}")
+        return func(url)
+    return wrapper
+
+
+def cache_result(func: Callable, expiration: int = 10) -> Callable:
+    """Caches the result of a function"""
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """Wraps called method and Caches the result"""
+        # Check if result is cached
+        cached_result = client.get(url)
+        if cached_result:
+            return cached_result.decode('utf-8')
+
+        # If not cached, call the function
+        result = func(url)
+
+        # Cache the result
+        client.setex(url, expiration, result)
+        return result
+    return wrapper
+
+
+@url_access_count
+@cache_result
+def get_page(url: str) -> str:
+    """GETs the HTML content of a particular URL and returns it"""
+    response = requests.get(url)
+    return response.text
