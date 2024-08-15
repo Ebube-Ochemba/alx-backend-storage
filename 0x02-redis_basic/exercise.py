8c4ee10881ae086f5e819326a34ac25d524ec085
Ebube-Ochemba <ebube17@gmail.com>
2024-08-15 04:09:51 +0100

    completed the Cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#/usr/bin/env python3
+"""A wrapper for Redis operations"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+
+    def __init__(self):
+        """Initialize redis client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, float, int, str]) -> str:
+        """Store data in redis"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
